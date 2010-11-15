d2a836c8ae7c66963fd389bbd2f7072a812d0810
heiko-braun <hbraun@redhat.com>
2010-11-15 14:44:55 +0100

    mavenize project, move source directories

--- src/net/froly/osw/client/Tokens.java
+++
@@ -1,9 +0,0 @@
-package net.froly.osw.client;
-
-public class Tokens {
-    public final static String HOME = "home";
-    public final static String MESSSAGES = "messages";
-    public final static String CONTACTS = "contacts";
-    public final static String SETTINGS = "settings";
-
-}
